36b853229e7eb7ac831a6e28dd8e6838856ed3fd
Michelle <michelledhuang@ucla.edu>
2019-04-22 22:36:47 -0700

    Implement VideoList and VideoListEntry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import {exampleVideoData} from '../data/exampleVideoData.js';
-import {VideoList} from './VideoList.jsx';
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
 var App = () => (
   <div>
     <nav className="navbar">
@@ -12,8 +12,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em><VideoList videos={exampleVideoData}/></h5></div>
-        <div>hello</div>
+        <VideoList videos={exampleVideoData}/>
       </div>
     </div>
   </div>
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,7 @@
+import VideoListEntry from './VideoListEntry.js';
 var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em>{props.videos[0]}</h5></div>
-    <div><h5><em>videoListEntry</em>{props.videos[1]}</h5></div>
-    <div><h5><em>videoListEntry</em>{props.videos[2]}</h5></div>
-    <div><h5><em>videoListEntry</em>{props.videos[3]}</h5></div>
-    <div><h5><em>videoListEntry</em>{props.videos[4]}</h5></div>
+    {props.videos.map((video, i) => (<VideoListEntry video={video} key={i}/>))}
   </div>
 );
 
@@ -13,8 +10,6 @@ var VideoList = (props) => (
 VideoList.propTypes = {
   videos: React.PropTypes.array.isRequired
 };
-
-// ReactDOM.render(<VideoList videos={[exampleVideoData[0], exampleVideoData[1], exampleVideoData[2], exampleVideoData[3], exampleVideoData[4]]}/>, document.getElementById("app"));
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
